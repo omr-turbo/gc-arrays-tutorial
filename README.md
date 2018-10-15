11995b0f133c823b29ef4b1592441a45f273b185
Robert Young <rwy0717@gmail.com>
2018-10-15 16:04:08 -0400

    COMPLETE EXAMPLE
    Signed-off-by: Robert Young <rwy0717@gmail.com>

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 This is a skeleton project which participants will use as a starting point, as
 they follow along with the "Making an Object Model" tutorial.
 
+If you're here to do the exercises, check out these resources:
+
+1. worksheet.md: This is the work you'll be completing today.
+2. api-reference.md: A selective listing of APIs you'll be using to complete this tutorial
+3. slides.pdf: The slides from the workshop introduction.
--- main.cpp
+++ main.cpp
@@ -1,33 +1,48 @@
+#include <Splash/Allocators.hpp>
+#include <Splash/Barriers.hpp>
+
 #include <iostream>
 #include <chrono>
 #include <cstdlib>
 #include <cstdint>
 
-constexpr std::size_t ROOT_SIZE = 1000;
-constexpr std::size_t ITERATIONS = 1000000;
+constexpr std::size_t MAX_CHILD_SIZE =      1000;
+constexpr std::size_t ROOT_SIZE      =      1000;
+constexpr std::size_t ITERATIONS     = 100000000;
+constexpr std::size_t SLOT_STRIDE    =         3;
 
-__attribute__((noinline)) void
-gc_heap_version()
-{
-  // nothing
+constexpr std::size_t childSize(std::size_t i) {
+  return i % MAX_CHILD_SIZE;
 }
 
-__attribute__((noinline)) void
-c_heap_version()
-{
-  void** root = reinterpret_cast<void**>(std::calloc(ROOT_SIZE, sizeof(void*)));
-  for (std::size_t i = 0; i < ITERATIONS; i++) {
-    auto index = i % 1000;
-    auto size  = i % 1000;
-    void* child = std::calloc(size, 1);
-    if (root[index]) {
-      std::free(root[index]);
-    }
-    root[index] = child;
+constexpr std::size_t index(std::size_t i) {
+  return (i * SLOT_STRIDE) % ROOT_SIZE;
+}
+
+__attribute__((noinline))
+void gc_bench(OMR::GC::RunContext& cx) {
+  OMR::GC::StackRoot<Splash::RefArray> root(cx);
+  root = Splash::allocateRefArray(cx, ROOT_SIZE);
+  for (std::size_t i = 0; i < ITERATIONS; ++i) {
+    auto child = (Splash::AnyArray*)Splash::allocateBinArray(cx, childSize(i));
+    Splash::store(cx, *root, index(i), child);
   }
 }
 
-/// Call f(args), and returns the wallclock duration in microseconds.
+__attribute__((noinline))
+void malloc_bench() {
+  void** root = reinterpret_cast<void**>(std::calloc(ROOT_SIZE, sizeof(void*)));
+  for (std::size_t i = 0; i < ITERATIONS; i++) {
+    std::size_t idx = index(i);
+    void* child = std::malloc(childSize(i));
+    if (root[idx]) {
+      std::free(root[idx]);
+    }
+    root[idx] = child;
+  }
+}
+
+/// Call f(args), and returns the wallclock duration in seconds.
 template <typename F, typename... Args>
 double
 time(F&& f, Args&&... args)
@@ -35,14 +50,46 @@ time(F&& f, Args&&... args)
   auto start = std::chrono::high_resolution_clock::now();
   f(std::forward<Args>(args)...);
   auto end = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double, std::micro>  duration = end - start;
+  std::chrono::duration<double>  duration = end - start;
   return duration.count();
+}
+
+/// Call f(args) n times, and return the average wallclock duration in seconds
+template <typename F, typename... Args>
+double
+averageTime(const std::size_t n, F&& f, Args&&... args)
+{
+  if (n == 0) {
+    throw(std::invalid_argument("n must be greater than zero"));
+  }
+
+  double sum = 0;
+
+  for(std::size_t i = 0; i < n; ++i) {
+    sum += time(f, std::forward<Args>(args)...);
+  }
+
+  return sum / n;
+
 }
 
 extern "C" int
 main(int argc, char** argv)
 {
-  std::cout << "OMR: " << time(gc_heap_version) << std::endl;
-  std::cout << "C:   " << time(c_heap_version) << std::endl;
+  std::size_t n = 1;
+
+  OMR::Runtime runtime;
+  OMR::GC::System system(runtime);
+  OMR::GC::RunContext context(system);
+
+  double gc_time     = averageTime(n, gc_bench, context);
+  double malloc_time = averageTime(n, malloc_bench);
+  double diff = malloc_time - gc_time;
+
+  std::cout
+    << "GC:     " << gc_time     << " seconds" << "\n"
+    << "malloc: " << malloc_time << " seconds" << "\n"
+    << "diff:   " << diff        << " seconds" << std::endl;
+  
   return 0;
 }
